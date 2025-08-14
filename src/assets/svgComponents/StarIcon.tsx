9a55b2268c2f75f8a5271eab71de6dbd5673d7c1
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-14 11:35:21 +0900

    chore: React+Vite+Tailwind 초기세팅 설정

---
+++ src/assets/svgComponents/StarIcon.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgStarIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 16 15" {...props}><path fill="#fff" fillRule="evenodd" d="M7.234.623a.833.833 0 0 1 1.531 0l1.602 3.74a.83.83 0 0 0 .438.438l3.739 1.601a.833.833 0 0 1 0 1.532l-3.74 1.601a.83.83 0 0 0-.437.438l-1.602 3.74-.06.118a.835.835 0 0 1-1.41 0l-.061-.118-1.602-3.74a.83.83 0 0 0-.366-.403l-.072-.035-3.74-1.601a.833.833 0 0 1 0-1.532l3.74-1.601a.83.83 0 0 0 .438-.438zM6.78 4.855c-.21.492-.602.884-1.094 1.095L2.842 7.168l2.845 1.218c.491.21.883.603 1.094 1.095L8 12.325 9.218 9.48l.087-.18a2.1 2.1 0 0 1 1.007-.915l2.845-1.218-2.845-1.218a2.08 2.08 0 0 1-1.094-1.095L7.999 2.01z" clipRule="evenodd" /></svg>;
+export default SvgStarIcon;

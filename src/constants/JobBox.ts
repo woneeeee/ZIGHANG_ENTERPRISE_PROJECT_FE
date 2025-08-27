110c4ad6801fbc69479c618cb482607217e406ab
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-27 20:54:24 +0900

    feat: 직업 카테고리 박스 상수 분리

---
+++ src/constants/JobBox.ts
@@ -0,0 +1,135 @@
+interface JobBox {
+  name: string
+  url: string
+  icon: string
+}
+
+const JobBoxConstant: JobBox[] = [
+  {
+    name: 'IT·개발',
+    url: '/it',
+    icon: 'https://zighang.com/depth1-renew/computer.svg',
+  },
+  {
+    name: 'AI·데이터',
+    url: '/ai',
+    icon: 'https://zighang.com/depth1-renew/memory.svg',
+  },
+  {
+    name: '게임',
+    url: '/game',
+    icon: 'https://zighang.com/depth1-renew/sports-esports.svg',
+  },
+  {
+    name: '디자인',
+    url: '/design',
+    icon: 'https://zighang.com/depth1-renew/design-services.svg',
+  },
+  {
+    name: '기획·전략',
+    url: '/strategy',
+    icon: 'https://zighang.com/depth1-renew/assignment-purple.svg',
+  },
+  {
+    name: '마케팅·광고',
+    url: '/marketing',
+    icon: 'https://zighang.com/depth1-renew/storefront.svg',
+  },
+  {
+    name: '상품기획·MD',
+    url: '/md',
+    icon: 'https://zighang.com/depth1-renew/sell.svg',
+  },
+  {
+    name: '영업',
+    url: '/sales',
+    icon: 'https://zighang.com/depth1-renew/point-of-sale-blue.svg',
+  },
+  {
+    name: '무역·물류',
+    url: '/logistics',
+    icon: 'https://zighang.com/depth1-renew/warehouse.svg',
+  },
+  {
+    name: '운송·배송',
+    url: '/driver',
+    icon: 'https://zighang.com/depth1-renew/storefront.svg',
+  },
+  {
+    name: '법률·법무',
+    url: '/legal',
+    icon: 'https://zighang.com/depth1-renew/balance.svg',
+  },
+  {
+    name: 'HR·총무',
+    url: '/hr',
+    icon: 'https://zighang.com/depth1-renew/people.svg',
+  },
+  {
+    name: '회계·재무·세무',
+    url: '/accounting',
+    icon: 'https://zighang.com/depth1-renew/table-view.svg',
+  },
+  {
+    name: '증권·운용',
+    url: '/finance',
+    icon: 'https://zighang.com/depth1-renew/query-stats.svg',
+  },
+  {
+    name: '은행·카드·보험',
+    url: '/bank',
+    icon: 'https://zighang.com/depth1-renew/account-balance.svg',
+  },
+  {
+    name: '엔지니어링·R&D',
+    url: '/research',
+    icon: 'https://zighang.com/depth1-renew/architecture.svg',
+  },
+  {
+    name: '건설·건축',
+    url: '/construction',
+    icon: 'https://zighang.com/depth1-renew/apartment.svg',
+  },
+  {
+    name: '생산·기능직',
+    url: '/production',
+    icon: 'https://zighang.com/depth1-renew/assignment.svg',
+  },
+  {
+    name: '의료·보건',
+    url: '/medical',
+    icon: 'https://zighang.com/depth1-renew/vaccines.svg',
+  },
+  {
+    name: '공공·복지',
+    url: '/public',
+    icon: 'https://zighang.com/depth1-renew/volunteer-activism.svg',
+  },
+  {
+    name: '교육',
+    url: '/education',
+    icon: 'https://zighang.com/depth1-renew/school.svg',
+  },
+  {
+    name: '미디어·엔터',
+    url: '/media',
+    icon: 'https://zighang.com/depth1-renew/ondemand-video.svg',
+  },
+  {
+    name: '고객상담·TM',
+    url: '/customer',
+    icon: 'https://zighang.com/depth1-renew/point-of-sale.svg',
+  },
+  {
+    name: '서비스',
+    url: '/service',
+    icon: 'https://zighang.com/depth1-renew/handshake.svg',
+  },
+  {
+    name: '식음료',
+    url: '/food',
+    icon: 'https://zighang.com/depth1-renew/restaurant-menu.svg',
+  },
+]
+
+export default JobBoxConstant

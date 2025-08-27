9e1e8c903fc41038c7b98bf1f8c174e9e3d8272d
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-27 20:45:08 +0900

    feat: 필요한 svg 파일 추가

--- src/assets/svgComponents/index.ts
+++ src/assets/svgComponents/index.ts
@@ -1,1 +1,4 @@
+export { default as ChevronRightIcon } from './ChevronRightIcon'
+export { default as LogoIcon } from './LogoIcon'
+export { default as SearchIcon } from './SearchIcon'
 export { default as StarIcon } from './StarIcon'

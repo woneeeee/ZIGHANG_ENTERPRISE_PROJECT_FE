89e37e74cb45daf68dbf6498003b4b5d913a967d
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-28 18:02:41 +0900

    feat: Profile 파일 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Landing from '@/pages/Landing'
-// import Login from '@/pages/Login'
-// import Redirect from '@/pages/Redirect'
+import Profile from '@/pages/Profile'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* <Route path="/" element={<Login />} /> */}
-        {/* <Route path="/kakao/oauth" element={<Redirect />} /> */}
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )

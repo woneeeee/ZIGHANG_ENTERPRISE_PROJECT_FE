b1b2259cbb38a40785726b8916405c470d224524
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-30 22:29:59 +0900

    refactor: 랜딩페이지 반응형 추가

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -19,9 +19,9 @@ const Landing = () => {
   return (
     <main className="flex flex-col items-center justify-center">
       <Header />
-      <div className="flex w-full flex-col items-center pt-[40px]">
-        <div className="flex flex-col items-center gap-5">
-          <div className="flex items-center gap-1 text-[22px] font-bold">
+      <div className="tablet:pb-0 tablet:pt-[40px] relative w-full flex-col items-center pt-0 pb-0">
+        <div className="tablet:pt-0 flex w-full flex-col items-center gap-5 px-4 pt-5">
+          <div className="tablet:flex-row tablet:text-[22px] flex flex-col items-center gap-1 text-[18px] font-bold">
             <span>대기업 및 유니콘 채용 공고를</span>
             <div className="flex items-center">
               <span className="text-[#6F00B6]">빠짐없이 모두</span>
@@ -30,30 +30,32 @@ const Landing = () => {
           </div>
           <SearchInput />
         </div>
-        <div className="relative inline-flex items-center gap-2 py-[24px]">
-          <div
-            className="relative inline-flex flex-[0_0_auto] cursor-pointer items-center gap-1"
-            onClick={handleCompanyClick}
-          >
-            <p className="relative w-fit text-[16px] leading-[normal] font-semibold tracking-[0] whitespace-nowrap">
-              <span className="text-[#6F00B6]">111111개 기업</span>
-              <span className="text-[#646464]"> 전체 보기</span>
-            </p>
-            <ChevronRightIcon className="h-4 w-4 text-[#646464]" />
-          </div>
-          <div
-            className="relative inline-flex flex-[0_0_auto] cursor-pointer items-center gap-1 text-[16px]"
-            onClick={handleAllClick}
-          >
-            <p className="relative w-fit leading-[normal] font-semibold tracking-[0] whitespace-nowrap">
-              <span className="text-[#6F00B6]">11111개 공고</span>
-              <span className="text-[#646464]"> 전체 보기</span>
-            </p>
-            <ChevronRightIcon className="h-4 w-4 text-[#646464]" />
+        <div className="tablet:py-6 flex w-full items-center justify-center py-5">
+          <div className="tablet:gap-4 relative inline-flex items-center gap-2">
+            <div
+              className="relative inline-flex flex-[0_0_auto] cursor-pointer items-center gap-1"
+              onClick={handleCompanyClick}
+            >
+              <p className="tablet:text-base relative w-fit text-sm leading-[normal] font-semibold tracking-[0] whitespace-nowrap">
+                <span className="text-[#6F00B6]">111111개 기업</span>
+                <span className="text-[#646464]"> 전체 보기</span>
+              </p>
+              <ChevronRightIcon className="h-4 w-4 text-[#646464]" />
+            </div>
+            <div
+              className="relative inline-flex flex-[0_0_auto] cursor-pointer items-center gap-1"
+              onClick={handleAllClick}
+            >
+              <p className="tablet:text-base relative w-fit text-sm leading-[normal] font-semibold tracking-[0] whitespace-nowrap">
+                <span className="text-[#6F00B6]">11111개 공고</span>
+                <span className="text-[#646464]"> 전체 보기</span>
+              </p>
+              <ChevronRightIcon className="h-4 w-4 text-[#646464]" />
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center gap-9">
+      <div className="tablet:gap-9 tablet:px-[60px] tablet:pb-9 flex flex-col px-5 py-0">
         <JobBox />
         <Carousel />
       </div>

fad3d6ae68de503cab9e20236b37a258aedd02bb
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-30 13:18:24 +0900

    feat: 카카오 소셜 로그인 api 연결

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,5 +1,6 @@
+import api from '@/apis/api'
 import { useEffect } from 'react'
-import { useNavigate } from 'storybook/internal/router'
+import { useNavigate } from 'react-router-dom'
 
 const Redirect: React.FC = () => {
   const navigate = useNavigate()
@@ -7,9 +8,27 @@ const Redirect: React.FC = () => {
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search)
     const code = queryParams.get('code')
+    console.log(code)
 
     if (code) {
-      //api 연결 로직
+      api
+        .post('/auth/login/kakao', null, { params: { code } })
+        .then((res) => {
+          const { userId, accessToken, refreshToken } = res.data.result
+          localStorage.setItem('userId', userId)
+
+          if (res.data.code === '200') {
+            localStorage.setItem('accessToken', accessToken)
+            localStorage.setItem('refreshToken', refreshToken)
+            navigate('/profile')
+          } else {
+            navigate('/login')
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+          navigate('/login')
+        })
     }
   }, [navigate])
 

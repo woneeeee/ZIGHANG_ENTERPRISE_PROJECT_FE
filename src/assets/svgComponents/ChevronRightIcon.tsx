ba95851e43eda952e47cce4344dc8b17ec885786
김지원 <jiwonkim0531@gachon.ac.kr>
2025-09-15 02:07:46 +0900

    feat(#31): 필요한 캐릭터 및 아이콘 파일 추가

--- src/assets/svgComponents/ChevronRightIcon.tsx
+++ src/assets/svgComponents/ChevronRightIcon.tsx
@@ -8,7 +8,10 @@ const SvgChevronRightIcon = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 24 24"
     {...props}
   >
-    <path fill="#000" d="m16 12.112-7.112 7.113-1.326-1.326 5.787-5.787-5.787-5.787L8.888 5z" />
+    <path
+      fill="currentColor"
+      d="m16 12.112-7.112 7.113-1.326-1.326 5.787-5.787-5.787-5.787L8.888 5z"
+    />
   </svg>
 )
 export default SvgChevronRightIcon

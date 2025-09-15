47f5b89da572fc2567e152b3c5d9fa36e3d7cc0c
유림 <yulimh001@gmail.com>
2025-09-15 20:20:03 +0900

    feat: test api 연결

--- src/components/onboarding/test/Card.tsx
+++ src/components/onboarding/test/Card.tsx
@@ -6,18 +6,46 @@ interface CardProps {
   setStep: Dispatch<SetStateAction<number>>
 }
 export default function Card({ children, step, setStep }: CardProps) {
+  const renderImageUrl = (step: number) => {
+    switch (step) {
+      case 1:
+        return '/onboarding/company-size.png'
+      case 2:
+        return '/onboarding/partner-style.png'
+      case 3:
+        return '/onboarding/avoid-situation.png'
+      case 4:
+        return '/onboarding/motivation.png'
+      case 5:
+        return '/onboarding/after-work-choice.png'
+      default:
+        return '/onboarding/reward-choice.png'
+    }
+  }
+
   return (
-    <div className="desktop:w-[634px] laptop:w-[634px] tablet:w-[503px] w-[286px] flex flex-col items-center justify-center rounded-[30px] border border-[#78709E] desktop:px-[50px] laptop:px-[50px] desktop:py-[40px] laptop:py-[40px] tablet:py-[29px] tablet:px-[51px] py-[15px] px-[29px]">
-      <ProcessBar step={step}/>
-      <div className="tablet:w-[401px] laptop:w-[401px] desktop:w-[401px] w-[230px]">
-        {children}
-        <BottomButton step={step} setStep={setStep} />
+    <div className="desktop:w-[1085px] laptop:w-[1085px] desktop:flex desktop:gap-x-[90px] desktop:items-center desktop:justify-center laptop:flex laptop:gap-x-[90px] laptop:items-center laptop:justify-center">
+      <div className="hidden desktop:block laptop:block h-[474px] w-[492px]">
+        <img
+          alt="일러스트"
+          className={'desktop:rounded-[30px] laptop:rounded-[30px] rounded-[16px] object-cover'}
+          src={renderImageUrl(step)}
+        />
+      </div>
+      <div
+
+        className="tablet:w-[503px] desktop:px-[50px] laptop:px-[50px] desktop:py-[40px] laptop:py-[40px] tablet:py-[29px] tablet:px-[51px] desktop:gap-x-[90px] laptop:gap-x-[90px] flex w-[286px] items-center justify-center rounded-[30px] border bg-[#C8C2E512] border-[#78709E] px-[29px] py-[15px] ">
+        <div className="tablet:w-[401px] laptop:w-[401px] desktop:w-[401px] w-[230px]">
+          <ProcessBar step={step} />
+          {children}
+          <BottomButton step={step} setStep={setStep} />
+        </div>
       </div>
     </div>
   )
 }
 
-function ProcessBar({ step }: { step: number}) {
+function ProcessBar({ step }: { step: number }) {
   return (
     <div className="relative flex w-full flex-col gap-y-[10px]">
       <div className="h-[6px] rounded-full bg-purple-50"></div>
@@ -25,7 +53,7 @@ function ProcessBar({ step }: { step: number}) {
         className="absolute top-0 z-10 h-[6px] rounded-full bg-purple-300"
         style={{ width: `${(step / 6) * 100}%` }}
       />
-      <div className="w-full flex justify-end">
+      <div className="flex w-full justify-end">
         <p className="caption-sm-medium text-white">{step}/6</p>
       </div>
     </div>
@@ -40,7 +68,7 @@ function BottomButton({
   setStep: Dispatch<SetStateAction<number>>
 }) {
   return (
-    <div className="flex justify-start w-full">
+    <div className="flex w-full justify-start">
       {step !== 1 ? (
         <button
           onClick={() => {
@@ -50,7 +78,9 @@ function BottomButton({
         >
           이전으로
         </button>
-      ) : <div className="desktop:h-[44px] laptop:h-[44px] h-[30px] tablet:h-[44px]" />}
+      ) : (
+        <div className="desktop:h-[44px] laptop:h-[44px] tablet:h-[44px] h-[30px]" />
+      )}
     </div>
   )
 }

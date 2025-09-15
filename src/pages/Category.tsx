85720e432a2e2ddfcf6bf003eef68ded47aa15a5
김지원 <jiwonkim0531@gachon.ac.kr>
2025-09-15 18:07:47 +0900

    feat(#36): 공고 페이지에서 로켓 컴포넌트 설정

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,5 +1,6 @@
 import JobCard from '@/components/job/JobCard.tsx'
 import Header from '@/components/common/Header.tsx'
+import RocketAssistant from '@/components/rocket/RocketAssistant'
 
 export default function Category() {
   return (
@@ -8,7 +9,7 @@ export default function Category() {
       <div className="flex flex-col items-start">
         <section className="mt-8 flex gap-x-4">
           <button className="heading-md-medium border-b-[2px] pb-4">전체 공고</button>
-          <button className="heading-md-medium text-neutral-500 pb-4">저장한 공고</button>
+          <button className="heading-md-medium pb-4 text-neutral-500">저장한 공고</button>
         </section>
         {/* 필터 */}
         <section className="h-[60px] w-full"></section>
@@ -29,6 +30,12 @@ export default function Category() {
           <JobCard hasTag={false} />
         </section>
       </div>
+      <RocketAssistant
+        waypointSelector="#filter-itdev"
+        endLeft={50}
+        endBottom={100}
+        autoLaunch={true}
+      />
     </main>
   )
 }

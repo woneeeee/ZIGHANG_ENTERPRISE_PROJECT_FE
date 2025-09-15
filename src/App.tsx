673a31d2360f0e4a0fbc37c5fea5985e6523279b
유림 <yulimh001@gmail.com>
2025-09-15 13:35:29 +0900

    feat: 공고 메인 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import RootLayout from '@/layouts/RootLayout'
 import OnboardingLogin from '@/pages/onboarding/OnboardingLogin'
 import Start from '@/pages/onboarding/Start'
 import OnboardingTest from '@/pages/onboarding/OnboardingTest.tsx'
+import Job from '@/pages/Job.tsx'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/kakao/oauth" element={<Redirect />} />
+        <Route path="/job" element={<Job />} />
         <Route element={<RootLayout />}>
           <Route path="/onboarding/test" element={<OnboardingTest/>}></Route>
           <Route path="/onboarding/start" element={<Start />} />

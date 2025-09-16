07a251f64416e86161afec7152a94504ded69bb1
김지원 <jiwonkim0531@gachon.ac.kr>
2025-09-16 23:53:25 +0900

    remove(#52): 제거된 마이페이지 필드 타입 삭제

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,13 +1,11 @@
 export type Job = {
   jobGroups: string
   jobPositions: []
-  companyTypes: []
   education: string
   workExperience: number
   address: string
   transport: string
   maxCommuteMinutes: number
-  receivingEmail: string
 }
 
 export type Profile = {

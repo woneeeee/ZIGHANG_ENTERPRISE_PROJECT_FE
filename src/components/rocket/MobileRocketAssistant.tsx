3323e9f82bcb389a7350889a1060e289530df0c7
김지원 <jiwonkim0531@gachon.ac.kr>
2025-09-16 23:50:40 +0900

    feat(#52): 모바일로켓 컴포넌트 구현

---
+++ src/components/rocket/MobileRocketAssistant.tsx
@@ -0,0 +1,50 @@
+import { motion, useReducedMotion } from 'framer-motion'
+import Lottie from 'lottie-react'
+import rocketClickTakeoff from '@/assets/lotties/rocket_click_takeoff.json'
+import RocketText from './RocketText'
+import { useNavigate } from 'react-router-dom'
+
+type Props = {
+  size?: number
+  amplitude?: number
+  period?: number
+  className?: string
+}
+
+export default function MobileRocketAssistant({
+  size = 120,
+  amplitude = 6,
+  period = 1.6,
+  className,
+}: Props) {
+  const reduce = useReducedMotion()
+  const amp = reduce ? 0 : amplitude
+  const nav = useNavigate()
+
+  return (
+    <motion.div
+      animate={{ y: [-amp, amp] }}
+      transition={{
+        duration: period,
+        repeat: Infinity,
+        repeatType: 'mirror',
+        ease: 'easeInOut',
+      }}
+      onClick={() => {
+        nav('/onboarding/start')
+      }}
+      className={`flex cursor-pointer flex-col items-center justify-center ${className}`}
+      style={{ width: size, height: size }}
+    >
+      <Lottie
+        animationData={rocketClickTakeoff}
+        loop
+        autoplay
+        className="ml-[50px] h-full w-full"
+      />
+      <div className="absolute -bottom-1 left-1/2 -translate-x-1/2">
+        <RocketText />
+      </div>
+    </motion.div>
+  )
+}

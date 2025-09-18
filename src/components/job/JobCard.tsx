f1fd6f7ba6a0f7546ee9df186952501317bf86c7
김지원 <jiwonkim0531@gachon.ac.kr>
2025-09-18 14:46:51 +0900

    HOTFIX: 복지와 시간 선택 props로 수정

--- src/components/job/JobCard.tsx
+++ src/components/job/JobCard.tsx
@@ -13,7 +13,11 @@ export default function JobCard({ hasTag = true, hasViewCount = false, job }: Jo
    * @param minutes - 변환할 분 수
    * @returns "X시간 Y분" 형식의 문자열
    */
-  function convertMinutesToHoursAndMinutes(minutes: number): string {
+  function convertMinutesToHoursAndMinutes(minutes: number | undefined): string {
+    if (minutes == undefined) {
+      throw new Error('minutes이 undefined입니다.')
+    }
+
     if (minutes < 0) {
       throw new Error('분은 음수일 수 없습니다.')
     }
@@ -34,8 +38,8 @@ export default function JobCard({ hasTag = true, hasViewCount = false, job }: Jo
 
   return (
     <div className="tablet:rounded-[26px] laptop:rounded-[26px] desktop:rounded-[26px] flex w-full rounded-[10px] shadow-[0_0_12.733px_0_rgba(0,0,0,0.08)]">
-      <section className="desktop:h-[160px] laptop:h-[160px] tablet:h-[160px] h-[104px] tablet:gap-x-[24px] desktop:gap-x-[24px] laptop:gap-x-[24px] desktop:py-[16px] desktop:px-6 laptop:py-[16px] laptop:px-6 tablet:py-[16px] tablet:px-6 tablet:rounded-l-[26px] laptop:rounded-l-[26px] desktop:rounded-l-[26px] flex w-full items-center gap-x-[10px] rounded-l-[10px] border-y border-l border-neutral-200 bg-white py-[14px] pl-[10px]">
-        <div className="tablet:h-[80px] tablet:w-[80px] laptop:w-[70px] laptop:h-[70px] caption-md-medium tablet:min-w-[80px] laptop:min-w-[70px] desktop:min-w-[80px] tablet:min-h-[80px] laptop:min-h-[70px] desktop:min-h-[80px] flex min-h-[55px] min-w-[55px] w-[55px] h-[55px] items-center justify-center truncate rounded-[12px] bg-purple-400 desktop:px-3 laptop:px-3 tablet:px-3 px-2  text-white">
+      <section className="desktop:h-[160px] laptop:h-[160px] tablet:h-[160px] tablet:gap-x-[24px] desktop:gap-x-[24px] laptop:gap-x-[24px] desktop:py-[16px] desktop:px-6 laptop:py-[16px] laptop:px-6 tablet:py-[16px] tablet:px-6 tablet:rounded-l-[26px] laptop:rounded-l-[26px] desktop:rounded-l-[26px] flex h-[104px] w-full items-center gap-x-[10px] rounded-l-[10px] border-y border-l border-neutral-200 bg-white py-[14px] pl-[10px]">
+        <div className="tablet:h-[80px] tablet:w-[80px] laptop:w-[70px] laptop:h-[70px] caption-md-medium tablet:min-w-[80px] laptop:min-w-[70px] desktop:min-w-[80px] tablet:min-h-[80px] laptop:min-h-[70px] desktop:min-h-[80px] desktop:px-3 laptop:px-3 tablet:px-3 flex h-[55px] min-h-[55px] w-[55px] min-w-[55px] items-center justify-center truncate rounded-[12px] bg-purple-400 px-2 text-white">
           <div className="truncate">{job.companyName}</div>
         </div>
         <div className="tablet:gap-y-2 laptop:gap-y-2 desktop:gap-y-2 desktop:w-[60%] laptop:w-[80%] flex flex-col gap-y-[6px]">
@@ -47,7 +51,7 @@ export default function JobCard({ hasTag = true, hasViewCount = false, job }: Jo
             </div>
             {hasTag ? (
               <>
-                <div className="flex h-fit w-fit items-center gap-x-1 rounded-[2px] bg-[#F0F5FF] px-[2px] desktop:p-1 laptop:p-1 tablet:p-1">
+                <div className="desktop:p-1 laptop:p-1 tablet:p-1 flex h-fit w-fit items-center gap-x-1 rounded-[2px] bg-[#F0F5FF] px-[2px]">
                   <BusIcon width={14} height={14} />
                   <p className="caption-xs-semibold tablet:body-caption-sm-medium laptop:body-caption-sm-medium desktop:body-caption-sm-medium text-[#0057FF]">
                     {convertMinutesToHoursAndMinutes(job.commuteMinutes)}
@@ -103,7 +107,7 @@ export default function JobCard({ hasTag = true, hasViewCount = false, job }: Jo
       </section>
 
       <section className="tablet:rounded-r-[26px] laptop:rounded-r-[26px] desktop:rounded-r-[26px] flex items-center justify-center rounded-r-[10px] border-y border-r border-neutral-200">
-        <div className="desktop:h-[134px] laptop:h-[134px] tablet:h-[134px] h-[88px] desktop:px-[30px] laptop:px-[30px] tablet:px-[30px] px-[15px] flex items-center justify-center border-l border-neutral-200">
+        <div className="desktop:h-[134px] laptop:h-[134px] tablet:h-[134px] desktop:px-[30px] laptop:px-[30px] tablet:px-[30px] flex h-[88px] items-center justify-center border-l border-neutral-200 px-[15px]">
           <BookmarkIcon
             width={20}
             height={20}
--- src/types/job.ts
+++ src/types/job.ts
@@ -9,8 +9,22 @@ export interface JobPostingType {
   workExperience: string
   recruitmentType: RecruitmentTypeEnum[] //계약직
   education: string
-  commuteMinutes: number
-  welfare: string //복지
+  commuteMinutes?: number
+  welfare?: string //복지
 }
-export type RecruitmentTypeKor = '정규직' | '계약직' | '일용직' | '전환형 인턴' | '체험형 인턴' | '프리랜서' | '산업기능요원'
-export type RecruitmentTypeEnum = 'FULL_TIME' | 'CONTRACT' | 'DAY_WORKER' | 'CONVERTIBLE_INTERN' | 'EXPERIENTIAL_INTERN' | 'FREELANCER' | 'ALTERNATIVE_MILITARY_SERVICE'
+export type RecruitmentTypeKor =
+  | '정규직'
+  | '계약직'
+  | '일용직'
+  | '전환형 인턴'
+  | '체험형 인턴'
+  | '프리랜서'
+  | '산업기능요원'
+export type RecruitmentTypeEnum =
+  | 'FULL_TIME'
+  | 'CONTRACT'
+  | 'DAY_WORKER'
+  | 'CONVERTIBLE_INTERN'
+  | 'EXPERIENTIAL_INTERN'
+  | 'FREELANCER'
+  | 'ALTERNATIVE_MILITARY_SERVICE'

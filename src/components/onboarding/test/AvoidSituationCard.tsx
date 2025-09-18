9f1d4c787c02946236d6819ff478b1f3e21854a1
유림 <yulimh001@gmail.com>
2025-09-18 17:01:33 +0900

    fix(#96): 테스트 페이지 디자인 수정 완료

--- src/components/onboarding/test/AvoidSituationCard.tsx
+++ src/components/onboarding/test/AvoidSituationCard.tsx
@@ -28,9 +28,11 @@ export default function AvoidSituationCard ({step, setStep}: AvoidSituationCardP
             src={'/onboarding/avoid-situation.png'}
           />
         </div>
-        <h1 className="desktop:mt-[46px] laptop:mt-[46px] body-sm-semibold tablet:heading-md-semibold laptop:heading-md-semibold desktop:heading-md-semibold mt-[21px] text-center text-white">
-          Q. 광활한 우주 속 가장 두려운 상황은?
-        </h1>
+        <div className="flex items-center justify-center desktop:mt-[46px] laptop:mt-[46px] h-[64px] mt-[21px]">
+          <h1 className="body-sm-semibold tablet:heading-md-semibold laptop:heading-md-semibold desktop:heading-md-semibold text-center text-white">
+            Q. 광활한 우주 속 가장 두려운 상황은?
+          </h1>
+        </div>
         <div className="desktop:mt-[46px] desktop:mb-[30px] laptop:mt-[46px] laptop:mb-[30px] tablet:my-[38px] desktop:gap-y-6 laptop:gap-y-6 tablet:gap-y-6 my-[14px] flex w-full flex-col gap-y-3">
           {contentList.map((content) => {
             return (
@@ -43,7 +45,7 @@ export default function AvoidSituationCard ({step, setStep}: AvoidSituationCardP
                   setStep(4)
                 }}
                 key={content.enum}
-                className="bg-[#EEF0F9] hover:bg-purple-200 transition laptop:body-2xl-medium desktop:body-2xl-medium tablet:body-2xl-medium caption-md-medium desktop:h-[60px] laptop:h-[60px] tablet:h-[60px] desktop:px-[30px] desktop:py-[16px] laptop:px-[30px] laptop:py-[16px] tablet:px-[30px] tablet:py-[16px] flex h-[36px] w-full items-center rounded-[5px] px-[16px] py-[10px]"
+                className="focus:bg-purple-400 focus:text-white laptop:body-2xl-medium desktop:body-2xl-medium tablet:body-2xl-medium caption-md-medium desktop:h-[60px] laptop:h-[60px] tablet:h-[60px] desktop:px-[30px] desktop:py-[16px] laptop:px-[30px] laptop:py-[16px] tablet:px-[30px] tablet:py-[16px] flex h-[36px] w-full items-center rounded-[5px] bg-[#EEF0F9] px-[16px] py-[10px] transition hover:bg-purple-200"
               >
                 {content.kor}
               </button>
